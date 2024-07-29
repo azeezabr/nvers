1cc47760d7654b8c90cd5ab8c54373e057779900
azeezabr001@gmail.com <azeezabr001@gmail.com>
2024-07-29 06:45:15 +0000

    added silver table with scoped credentials

---
+++ silver_customer_profile.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+spark._jsc.hadoopConfiguration().set("fs.azure.account.key.degroup1.dfs.core.windows.net", dbutils.secrets.get('nvers','SID')) 
+
+# COMMAND ----------
+
+storage_name = dbutils.secrets.get('nvers','storage_name')
+container_name = dbutils.secrets.get('nvers','container_name')
+adls_raw_folder = f"abfss://{container_name}@{storage_name}.dfs.core.windows.net"
+
+# COMMAND ----------
+
+df = spark.read.parquet(f'{adls_raw_folder}/overview')
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+
